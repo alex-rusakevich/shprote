5b75daa11660871d15692e94d4c1baf7675cecf5
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2023-05-17 12:23:16 +0300

    heroku config file dyno fix

--- shprote/config.py
+++ shprote/config.py
@@ -7,10 +7,13 @@ config = {}
 
 def load_config(config_path=os.path.join(f".", "config.toml"),
                 default_config_path=os.path.join(".", "default_config.toml")) -> dict[any, any]:
+    global config
+    if os.environ.get("DYNO"):
+        config = toml.load(default_config_path)
+        return config
+
     if not (os.path.exists(config_path) and os.path.isfile(config_path)):
         shutil.copy(default_config_path, config_path)
-
-    global config
     config = toml.load(config_path)
     return config
 
