9828077fc6d27c824b2501005e8e71208c44f542
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2023-05-20 17:57:00 +0300

    Fixing connection dropped mysql error

--- shprote/db/__init__.py
+++ shprote/db/__init__.py
@@ -10,14 +10,21 @@ config = get_config()
 DATABASE = {}
 DB_ENGINE = None
 
+db_options = config["database"]["options"]
+db_options = {
+    "pool_pre_ping": db_options["pre-ping"],
+    "pool_recycle": db_options["recycle"]
+}
+
 if "CLEARDB_DATABASE_URL" in os.environ:
     DATABASE = os.environ["CLEARDB_DATABASE_URL"].replace(
         "?reconnect=true", "")
-    DB_ENGINE = create_engine(DATABASE, echo=config["main"]["debug"])
+    DB_ENGINE = create_engine(
+        DATABASE, echo=config["main"]["debug"], **db_options)
 else:
-    DATABASE = config["database"]
+    DATABASE = config["database"]["connection"]
     DB_ENGINE = create_engine(URL.create(**DATABASE),
-                              echo=config["main"]["debug"])
+                              echo=config["main"]["debug"], **db_options)
 
 DeclarativeBase.metadata.create_all(DB_ENGINE)
 DB_SESSION_FACTORY = sessionmaker(bind=DB_ENGINE)
