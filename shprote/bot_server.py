a0b844f618b801eb0331c4638d111b33fb2880a8
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2023-05-21 15:46:04 +0300

    Fixed typo

--- shprote/bot_server.py
+++ shprote/bot_server.py
@@ -16,7 +16,7 @@ Created by Alexander Rusakevich (https://github.com/alex-rusakevich)
 
 /start — start the bot and go to the main menu
 /help — display this message
-/check — begin your pronunciation test. The special code / hash in bot replies is intended to avoid pupil cheating by sending different results or fake tasks
+/checkpr — begin your pronunciation test. The special code / hash in bot replies is intended to avoid pupil cheating by sending different results or fake tasks
 /stop — stop the test and go to main menu
 
 _May 汉语 be with you!_
