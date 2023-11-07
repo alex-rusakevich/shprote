8b1a0c912c85db58e8afa44aa29f594f71897991
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2023-11-07 15:09:17 +0300

    #patch bumped version

--- shprote/__init__.py
+++ shprote/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Alexander Rusakevich"
-__adress__ = 'https://github.com/alex-rusakevich/shprote'
+__adress__ = "https://github.com/alex-rusakevich/shprote"
 
-__version_info__ = (0, 7, 5)
+__version_info__ = (0, 8, 0)
 __version_suffix__ = ""
 __version__ = "v" + ".".join(map(str, __version_info__)) + __version_suffix__
