23ea036b58eeb088b6e5aebeb0e83c9f25178466
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2024-05-12 13:25:08 +0800

    #patch added GNUv3 preamble

--- shprote/db/management.py
+++ shprote/db/management.py
@@ -1,3 +1,19 @@
+# Shprote bot - Standardized Hanyu (Chinese) PROnunciation TEster
+# Copyright (C) 2023, 2024 Alexander Rusakevich
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from expiringdict import ExpiringDict
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.sql import func
--- tests/test_zh.py
+++ tests/test_zh.py
@@ -1,3 +1,19 @@
+# Shprote bot - Standardized Hanyu (Chinese) PROnunciation TEster
+# Copyright (C) 2023, 2024 Alexander Rusakevich
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import pytest
 
 from shprote.logics import *
@@ -7,7 +23,12 @@ from shprote.logics.zh import compare_lang_text, text_to_levenseq
 test_data = (
     ("码", "妈", 0.5000, 1),
     ("你好，我叫萨沙", "你好，我叫米沙", 0.8750, 2),
-    ("我们那儿有个王小三儿，在门口儿摆着一个小杂货摊儿", "我们那儿有个王小三儿，在门口儿白着一个小杂货摊儿", 0.9828, 1),
+    (
+        "我们那儿有个王小三儿，在门口儿摆着一个小杂货摊儿",
+        "我们那儿有个王小三儿，在门口儿白着一个小杂货摊儿",
+        0.9828,
+        1,
+    ),
     ("画儿", "画", 0.7500, 1),
     ("儿子", "二字", 0.5000, 2),
     ("妈妈", "密码", 0.5000, 2),
